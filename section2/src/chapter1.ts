9c2ef474ec2a27fc28cfc03813e98d4fec1d58c4
donghoonlee94 <94donghoon.lee@gmail.com>
2024-09-10 13:43:03 +0900

    primitive & literal type

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,41 @@
+// number
+let num: number = 123
+let num1 :number = -123
+let num2 : number = 0.123
+let num3 : number  = Infinity
+let num4 : number = -Infinity
+let num5 : number = NaN
+
+num = 555
+
+// string 
+let str1 : string = 'hellp'
+
+str1 = 'asdf'
+
+// boolean
+
+let boo1 : boolean = true
+let boo2 : boolean = false
+
+// null 
+
+let null1 : null = null
+
+// undefined
+
+let unde1: undefined = undefined
+
+
+// strict null checks false 
+// let numA : number = null
+
+// 리터럴 타입
+
+let numA : 10 = 10
+
+
+let strA : 'string' = 'string'
+
+let booA : true = true
+
