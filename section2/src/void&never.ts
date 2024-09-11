0c34aa569e1259ef34b36fbc2693b48f9dd93679
donghoonlee94 <94donghoon.lee@gmail.com>
2024-09-11 15:16:29 +0900

    downcasting & upcasting

--- section2/src/void&never.ts
+++ section2/src/void&never.ts
@@ -25,3 +25,20 @@ function func3 (): never {
 function func4 (): never {
     throw new Error()
 }
+
+let b : never;
+
+// b = 1; 
+// b = undefined
+// b = null 
+
+
+
+let num1: 10 = 10
+let num2: number = 20 
+
+// upcasting
+// O  num2 = num1 
+
+// downcasting
+// X  num1 = num2
