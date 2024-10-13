d6117080e753883a372018cdc922ae6961584c3d
donghoonlee <94donghoon.lee@gmail.com>
2024-10-13 17:10:23 +0900

    Context API with typescript

--- section11/src/components/Editor.tsx
+++ section11/src/components/Editor.tsx
@@ -1,11 +1,13 @@
 import { ReactElement, useState } from "react";
+import { useTodoDispatch } from "../App";
 
 interface Props {
-  onClickAdd: (text: string) => void;
   children?: ReactElement;
 }
 
 export default function Editor(props: Props) {
+  const dispatch = useTodoDispatch();
+
   const [text, setText] = useState("");
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -13,7 +15,7 @@ export default function Editor(props: Props) {
   };
 
   const onClickButton = () => {
-    props.onClickAdd(text);
+    dispatch.onClickAdd(text);
     setText("");
   };
 
