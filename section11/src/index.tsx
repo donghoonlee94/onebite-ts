cc7501fc1c6059a7c3d38a64bf44f7a3210ea5c5
donghoonlee <94donghoon.lee@gmail.com>
2024-10-13 17:15:13 +0900

    외부 라이브러리 타입

--- section11/src/index.tsx
+++ section11/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import _ from "lodash";
 
 // Null 타입을 반환할 수도 있는데, createdRoot는 null 타입을 받지 않음.
 // non null ! or as HTMLElement로 해결 가능.
